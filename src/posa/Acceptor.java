067a29bfcafc0c47385b41b5b3ebabf2dc07d417
Matveyich <matveyich@gmail.com>
2013-04-24 00:49:31 +0300

    init

---
+++ src/posa/Acceptor.java
@@ -0,0 +1,6 @@
+package posa;
+
+public class Acceptor {
+
+
+}
---
+++ src/posa/EchoHandler.java
@@ -0,0 +1,8 @@
+package posa;
+
+public class EchoHandler {
+	public String processInput(String inputLine){
+		System.out.println(inputLine);
+		return inputLine;		
+	}
+}
